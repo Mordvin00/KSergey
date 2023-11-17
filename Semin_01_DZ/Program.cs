001eeff970e9bf43b83ca6f1a24ca74a916c8354
Mordvin <mordvin00@gmail.com>
2023-11-17 23:53:56 +0300

    add Semin_01_DZ - Добавил решение 1-го задания ДЗ по работе с С#

---
+++ Semin_01_DZ/Program.cs
@@ -0,0 +1,79 @@
+﻿/*using System;
+
+public class Answer {
+    static void CompareNumbers(int firstNumber, int secondNumber)
+    {
+        // Введите свое решение ниже
+ if (firstNumber < secondNumber)
+                    Console.WriteLine($"Первое число `{firstNumber}` меньше чем второе число `{secondNumber}`");
+               else
+        if (firstNumber > secondNumber)
+                    Console.WriteLine($"Первое число `{firstNumber}` больше чем второе число `{secondNumber}`");
+        else
+            Console.WriteLine($"Введенные числа равны `{firstNumber}`");
+
+    }
+
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int firstNumber, secondNumber;
+
+        if (args.Length >= 2) {
+            firstNumber = int.Parse(args[0]);
+            secondNumber = int.Parse(args[1]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            firstNumber = 5;
+            secondNumber = 5;
+        }
+
+        // Не удаляйте строки ниже
+        CompareNumbers(firstNumber, secondNumber);
+    }
+}
+*/
+
+using System;
+
+public class Answer
+{
+
+    static int FindMax(int a, int b, int c)
+    {
+        // Введите свое решение ниже
+
+        if (a > b)
+            Console.WriteLine($"a");
+        else
+        if (b > c)
+            Console.WriteLine($"b");
+        else
+            Console.WriteLine($"c");
+
+    }
+
+    // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args)
+    {
+        int a, b, c;
+
+        if (args.Length >= 3)
+        {
+            a = int.Parse(args[0]);
+            b = int.Parse(args[1]);
+            c = int.Parse(args[2]);
+        }
+        else
+        {
+            // Здесь вы можете поменять значения для отправки кода на Выполнение
+            a = 5;
+            b = 6;
+            c = 7;
+        }
+
+        // Не удаляйте строки ниже
+        int result = FindMax(a, b, c);
+        System.Console.WriteLine($"{result}");
+    }
+}
