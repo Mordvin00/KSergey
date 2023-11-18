74a2af1f273aaf69f6787bde3121d128cb29e50e
Mordvin <mordvin00@gmail.com>
2023-11-19 00:21:35 +0300

    add Semin_01_DZ - Добавил решение 3-го задания ДЗ по работе с С#

--- Semin_01_DZ/Program.cs
+++ Semin_01_DZ/Program.cs
@@ -95,27 +95,57 @@ public class Answer
 
 using System;
 
-class Answer {
+/*
+Внутри класса Answer напишите метод CheckIfEven, который на вход принимает число number и выводит,
+является ли число чётным (делится ли оно на два без остатка).
+Пример использования:
+На входе:
+number: '6'
+На выходе:
+Число `6` чётное
+На входе:
+number: '3'
+На выходе:
+Число `3` нечётное
+*/
+class Answer
+{
     static void CheckIfEven(int number)
     {
-      // Введите свое решение ниже
+        // Введите свое решение ниже
 
+        if (number % 2 > 0)
+            Console.WriteLine($"Число `{number}` нечётное");
+        else
+            Console.WriteLine($"Число `{number}` чётное");
 
-    }
-  
+        // if ((number % 2) == 0)
+        // {
+        //     Console.WriteLine($"Число `{number}` чётное");
+        // }
+        // else
+        // {
+        //     Console.WriteLine($"Число `{number}` нечётное");
 
-  // Не удаляйте и не меняйте метод Main! 
-    static public void Main(string[] args) {
-        int number;
-
-        if (args.Length >= 1) {
-            number = int.Parse(args[0]);
-        } else {
-           // Здесь вы можете поменять значения для отправки кода на Выполнение
-            number = 6;
         }
 
-        // Не удаляйте строки ниже
-        CheckIfEven(number);
+
+        // Не удаляйте и не меняйте метод Main! 
+        static public void Main(string[] args)
+        {
+            int number;
+
+            if (args.Length >= 1)
+            {
+                number = int.Parse(args[0]);
+            }
+            else
+            {
+                // Здесь вы можете поменять значения для отправки кода на Выполнение
+                number = 6;
+            }
+
+            // Не удаляйте строки ниже
+            CheckIfEven(number);
+        }
     }
-}
