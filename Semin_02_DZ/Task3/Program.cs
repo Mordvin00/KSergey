4ba1fb984e811d2c99bc74ecff912c5aa22ef77b
Mordvin <mordvin00@gmail.com>
2023-11-24 04:11:30 +0300

    add Task3 - Добавил решение 3-го задания ДЗ по работе с С#

---
+++ Semin_02_DZ/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 3:
+Напишите программу, которая принимает на вход целое число
+из отрезка [10, 99] и показывает наибольшую цифру числа.*/
+
+Console.Clear();
+Console.Write("Введите двухзначное число: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 10 || n > 99)
+{
+    Console.Write("Данное число за пределами указанного отрезка!\nВведите двухзначное число: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+int firstNumber = n / 10;
+int secondNumber = n % 10;
+
+if (firstNumber == secondNumber)
+    Console.WriteLine($"Цифры числа равны и составляют: {firstNumber}");
+else
+if (firstNumber > secondNumber)
+    Console.WriteLine($"Наибольшая цифра - первая: {firstNumber}");
+else
+    Console.WriteLine($"Наибольшая цифра - вторая: {secondNumber}");
