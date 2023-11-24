444b057d1c997f15633de532f1b84b4efabecbca
Mordvin <mordvin00@gmail.com>
2023-11-24 03:18:45 +0300

    add Task2 - Добавил решение 2-го задания ДЗ по работе с С#

---
+++ Semin_02_DZ/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 2:
+Напишите программу, которая принимает на вход
+координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0
+и выдаёт номер координатной четверти плоскости,
+в которой находится эта точка.*/
+
+Console.Clear();
+Console.Write("Введите координату X: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату Y: ");
+int y = int.Parse(Console.ReadLine()!);
+if (x > 0 && y > 0)
+    Console.WriteLine("1");
+else if (x < 0 && y > 0)
+    Console.WriteLine("2");
+else if (x < 0 && y < 0)
+    Console.WriteLine("3");
+else if (x > 0 && y < 0)
+    Console.WriteLine("4");
+else
+    Console.WriteLine("Точка находится на оси координат!");
