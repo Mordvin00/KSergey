4b5ac54cb050b70cb66bc3513086596f28870f4e
Mordvin <mordvin00@gmail.com>
2023-11-26 01:15:24 +0300

    add Task2 - Добавил решение 2-го задания ДЗ 3-го семинара по работе с С#

---
+++ Semin_03_DZ/Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Задача 2: Задайте массив на 10 целых чисел.
+Напишите программу, которая определяет
+количество чётных чисел в массиве.
+Пример
+массив [6 7 19 34 3 1 4 7 9 1] => 3
+массив [1 8 43 4 55 60 3 2 1 3] => 4 */
+
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 101);
+    }
+    return array;
+}
+
+void PrintArray(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        Console.Write(inArray[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int lengthArray = 10;
+int[] arr = FillArray(lengthArray);
+PrintArray(arr);
+
+int i = 0;
+    foreach (int element in arr)
+    {
+        if(element % 2 == 0)
+        {
+            i++;
+        }
+    }
+        Console.WriteLine($"Количество чётных чисел в массиве: {i}");
