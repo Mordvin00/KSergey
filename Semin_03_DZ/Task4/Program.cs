3d60e575b9d8487b53851371650932df6858bdba
Mordvin <mordvin00@gmail.com>
2023-11-27 21:52:08 +0300

    add Semin_04 - Добавил записи преподавателя 4-го семинара по работе с С#

--- Semin_03_DZ/Task4/Program.cs
+++ Semin_03_DZ/Task4/Program.cs
@@ -9,7 +9,7 @@
 8741 => [8 7 4 1]
 4 => [4] */
 
-
+/*
 int[] FillArray(int size)
 {
     int[] array = new int[size];
@@ -33,4 +33,49 @@ Console.Write("Введите количество элементов масси
 int lengthArray = Convert.ToInt32(Console.ReadLine());
 int[] arr = FillArray(lengthArray);
 PrintArray(arr);
+*/
+
+
+void inputArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        arr[i] = new arr[i]; // [0; 9]
+}
+
+
+// int formingNumber(int[] array)
+// {
+//     int result = 0;
+//     int i = array.Length - 1;
+//     foreach (int el in array){
+//         result += el * Convert.ToInt32(Math.Pow(10, i));
+//         i--;
+//     }
+//     return result;
+// }
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 0 || n > 100000){
+    Console.Write("Вы ошиблись! Введите число от 0 до 100000: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine()!);
+while (n > 0){   //на вход принимает натуральное число N, а на выходе показывает его цифры через запятую
+    int x = n % 10;
+    Console.Write($"{inputArray(x)}, ");
+    n /= 10;
+}
+
+// int[] array = new int[n];
+// inputArray(array);
+// Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+// Console.WriteLine($"Результат: {formingNumber(array)}");
+// Console.WriteLine($"Результат: {string.Join("", array)}");
+
 
---
+++ Semin_04/Program.cs
@@ -0,0 +1,227 @@
+﻿//Семинар 4
+// Массивы(Array)
+
+// Список(List) <-> import array
+// data = [34, 12.5, "Ivan", True]
+// 34 - int
+// 12.5 - double
+// "Ivan" - string
+
+// int[] array = new int[5];
+// int[] array = {34, -10, 23, 9, 8};
+// array.Length = 5
+// [34, -10, 23, 9, 8]
+//  0    1   2   3  4
+// Console.WriteLine($"0-й элемент: {array[0]}");
+// Console.WriteLine($"1-й элемент: {array[1]}");
+// for (int i = 0; i < array.Length; i++)
+// {
+//     Console.Write($"{array[i]} ");
+// }
+// Console.WriteLine(string.Join(", ", array));
+
+// Console.Clear();
+// Console.Write("Введите кол-во элементов массива: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int[] array = new int[n];
+// Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(-10, 11); // [-10, 10]
+// }
+// Console.WriteLine($"Конечный массив: [{string.Join(", ", array)}]");
+
+
+// Console.Clear();
+// Console.Write("Введите кол-во элементов массива: ");
+// int n = int.Parse(Console.ReadLine()!);
+// double[] array = new double[n]; // 0.0
+// // Random().NextDouble() - (0, 1)
+// // от 20 до 30
+// // Random().NexDouble() * (end - start) + start;
+// Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = Math.Round(new Random().NextDouble() * (30 - 20) + 20, 2);
+// }
+// Console.WriteLine($"Конечный массив: [{string.Join(", ", array)}]");
+
+
+// Функции и процедуры
+
+// void test(ref int m) // процедура
+// {
+//     m = m + 10;
+// }
+
+
+// Console.Clear();
+// int n = 7;
+// test(ref n);
+// Console.WriteLine(n);
+
+
+// Напишите программу, которая принимает от пользователя 2 числа и возвращает наибольшее из них
+// int maxNumber(int a, int b){ // Функция
+//     if (a > b)
+//         return a;
+//     return b; // a <= b
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите 1-ое число: ");
+// int n = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите 2-ое число: ");
+// int m = int.Parse(Console.ReadLine()!);
+// Console.WriteLine($"Наибольшее значение: {maxNumber(n, m)}");
+
+
+
+// void inputArray(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//         arr[i] = new Random().Next(-10, 11); // [-10, 10]
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите кол-во элементов массива: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int[] array = new int[n];
+// Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+// inputArray(array);
+// Console.WriteLine($"Конечный массив: [{string.Join(", ", array)}]");
+
+
+// Задача 4: Напишите программу, которая на вход принимает натуральное число N, 
+// а на выходе показывает его цифры через запятую.
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine()!);
+// while (n > 0){
+//     int x = n % 10;
+//     Console.Write($"{x}, ");
+//     n /= 10;
+// }
+// 567 : 10 = 56 (ост. 7)
+// 567 : 10 = 56
+
+// 56 : 10 = 5(ост. 6)
+// 56 : 10 = 5
+
+
+// Задача 1
+// Задайте одномерный массив, заполненный случайными
+// числами. Определите количество простых чисел в этом
+// массиве.
+
+// void inputArray(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//         arr[i] = new Random().Next(1, 101); // [0, 100]
+// }
+
+// bool isPrime(int n){
+//     for (int i = 2; i <= n / 2; i++){
+//         if (n % i == 0)
+//             return false;
+//     }
+//     return true;
+// }
+// // 9 % 3 = 0
+// // 9:3
+// // 9:1
+// // 9:9
+// // int [217]
+
+// Console.Clear();
+// Console.Write("Введите кол-во элементов массива: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int[] array = new int[n];
+// inputArray(array);
+// Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+// Console.Write("Простые числа: ");
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (isPrime(array[i]) == true)
+//         Console.Write($"{array[i]} ");
+// }
+
+
+
+// Задача 2
+// Задайте массив из N случайных целых чисел (N вводится с клавиатуры).
+// Найдите количество чисел, которые оканчиваются на 1 и делятся нацело на 7.
+// Пример
+// [1 5 11 21 81 4 0 91 2 3] => 2
+
+// void inputArray(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//         arr[i] = new Random().Next(0, 101); // [-1000, 1000]
+// }
+
+
+// int isCheck(int[] array){
+//     int count = 0;
+//     foreach (int element in array){
+//         if (element % 10 == 1 && element % 7 == 0)
+//             count++;
+//     }
+//     return count;
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите кол-во элементов массива: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int[] array = new int[n];
+// inputArray(array);
+// Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+// Console.WriteLine($"Резльтат: {isCheck(array)}");
+
+
+// Задача 3
+// Заполните массив на N (вводится с консоли, не более 8)
+// случайных целых чисел от 0 до 9.
+// Сформируйте целое число, которое будет состоять из цифр из
+// массива. Старший разряд числа находится на 0-м индексе,
+// младший – на последнем.
+// Пример
+// [1 3 2 4 2 3] => 132423
+// [2 3 1] => 231
+
+
+void inputArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        arr[i] = new Random().Next(0, 10); // [0; 9]
+}
+
+
+int formingNumber(int[] array)
+{
+    int result = 0;
+    int i = array.Length - 1;
+    foreach (int el in array){
+        result += el * Convert.ToInt32(Math.Pow(10, i));
+        i--;
+    }
+    return result;
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 0 || n > 8){
+    Console.Write("Вы ошиблись! Введите число от 0 до 8: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+int[] array = new int[n];
+inputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Результат: {formingNumber(array)}");
+// Console.WriteLine($"Результат: {string.Join("", array)}");
