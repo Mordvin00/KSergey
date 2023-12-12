4fec60d84fa8f07d97834f36c2df589286a10354
Mordvin <mordvin00@gmail.com>
2023-12-12 19:56:49 +0300

    add Semin_06_DZ - Добавил ДЗ 6-го семинара по работе с С#

---
+++ Semin_06_DZ/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 1:
+Задайте двумерный массив символов (тип char [,]).
+Создать строку из символов этого массива.
+Пример
+a b c => “abcdef”
+d e f */
+
+
+{
+// Инициализация двумерного массива символов
+char[,] charArray = new char[,] { { 'a', 'b', 'c' }, { 'd', 'e', 'f' } };
+// Вызов метода для создания строки из 2D массива
+string result = CreateStringFrom2DArray(charArray);
+// Вывод результата
+Console.WriteLine(result);
+}
+// Метод для создания строки из двумерного массива символов
+static string CreateStringFrom2DArray(char[,] array)
+{
+string result = "";
+// Цикл по каждому элементу в двумерном массиве
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+// Добавление каждого символа в результирующую строку
+result += array[i, j];
+}
+}
+return result;
+}
---
+++ Semin_06_DZ/Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 3:
+Задайте произвольную строку.
+Выясните, является ли она палиндромом.
+Пример
+“aBcD1ef!-” => Нет
+“шалаш” => Да
+“55655” => Да */
+
+{
+// Входная строка для проверки
+// string input = "шалаш";
+Console.Clear();
+Console.Write("Введите слово для проверки, является ли оно палиндромом: ");
+string input = Console.ReadLine()!;
+// Вызов метода для проверки, является ли строка палиндромом
+bool isPalindrome = IsPalindrome(input);
+// Вывод результата
+Console.WriteLine(isPalindrome ? "Да" : "Нет");
+}
+// Метод для проверки, является ли строка палиндромом
+static bool IsPalindrome(string str)
+{
+// Нормализация строки путем удаления не буквенно-цифровыхсимволов и приведения к нижнему регистру
+string normalized = new string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+// Сравнение строки с ее перевернутым вариантом
+return normalized.SequenceEqual(normalized.Reverse());
+}
